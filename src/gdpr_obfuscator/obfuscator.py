34ab7f83f8986973b8802f3176d9d0abea423c6d
Oleksiieng <oleksii.eng@gmail.com>
2025-09-28 12:32:33 +0100

    fix(obfuscator): clearer error message when key missing

--- src/gdpr_obfuscator/obfuscator.py
+++ src/gdpr_obfuscator/obfuscator.py
@@ -15,7 +15,7 @@ def _get_key() -> bytes:
     key = os.getenv(KEY_ENV)
     if not key:
         raise RuntimeError(
-            f"Obfuscator key missing. Set ${KEY_ENV} environment variable."
+            f"Obfuscator key missing. Set {KEY_ENV} environment variable."
         )
     return key.encode("utf-8")
 
